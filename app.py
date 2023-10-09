897f90829b64bb4d85559c1ad1a41762cd806b84
София Лабаскина <sofialabaskina@gmail.com>
2023-10-09 15:10:24 +0700

    Роуты

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ def start():
     return redirect ("/menu", code=302)
 @app.route("/lab1")
 def lab1():
-    return """
+    return '''
 <!doctype html>
 <html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
     <head>
         <title> Лабаскина София, Переладова Алёна, Лабораторная 1 </title>
     </head>
@@ -25,20 +26,27 @@ def lab1():
         называемых микрофреймворков — минималистичных каркасов
         веб-приложений, сознательно предоставляющих лишь самые ба-
         зовые возможности.
-
+        <div><a href="/menu">Меню</a></div>
+        <h2>Реализованные роуты</h2>
+        <div><a href="/lab1/oak">Дуб</a></div>
+        <div><a href="/lab1/student">Студент</a></div>
+        <div><a href="/lab1/python">Пайтон</a></div>
+        <div><a href="/lab1/cat">Котик</a></div>
         <footer>
             &copy; Лабаскина София, Переладова Алёна, ФБИ-12, 3 курс, 2023
         </footer>
     </body>
 </html>
-"""
+'''
 @app.route("/menu")
 def menu():
-    return """
+    return '''
 <!doctype html>
 <html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
     <head>
         <title> Лабаскина София, Переладова Алёна, Лабораторная 1 </title>
+        
     </head>
     <body>
         <header>
@@ -52,7 +60,7 @@ def menu():
         </footer>
     </body>
 </html>
-"""
+'''
 @app.route('/lab1/oak')
 def oak():
     return '''
@@ -62,6 +70,67 @@ def oak():
     <body>
         <h1>Дуб</h1>
         <img src="''' + url_for('static', filename='oak.jpg') + '''">
+    </body>
+</html>
+'''
+@app.route('/lab1/student')
+def student():
+    return '''
+<!doctype html>
+<html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    <body>
+        <h1>Студент: Лабаскина София, Переладова Алёна</h1>
+        <img src="''' + url_for('static', filename='logo.png') + '''">
+    </body>
+</html>
+'''
+@app.route('/lab1/python')
+def python():
+    return '''
+<!doctype html>
+<html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    <body>
+        <h1>Знали ли вы?</h1>
+        <div> Имя Python происходит от Монти Пайтона.
+        Когда Гвидо ван Россум создавал Python, он также читал
+        сценарии из "Летающего цирка Монти Пайтона".
+        Он подумал, что такое имя было подходящим, коротким и немного загадочным.</div>
+        <h1>Для чего используется Python?</h1>
+        <div>Python обычно используется для разработки веб-сайтов
+        и программного обеспечения, автоматизации задач,
+        анализа данных и визуализации данных.
+        Поскольку его относительно легко выучить, Python был принят многими
+        непрограммистами, такими как бухгалтеры и ученые,
+        для выполнения различных повседневных задач, 
+        таких как организация финансов.</div>
+        <img src="''' + url_for('static', filename='py.jpg') + '''">
+    </body>
+</html>
+'''
+@app.route('/lab1/cat')
+def cat():
+    return '''
+<!doctype html>
+<html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    <body>
+        <h1>Знали ли вы?</h1>
+        <div>Кошки хорошо ориентируются во вкусах, 
+        различают кислое, горькое и солёное. 
+        Разборчивость эта обусловлена, прежде всего, хорошим нюхом 
+        и развитыми вкусовыми рецепторами на языке. Долгое время считалось, 
+        что в отличие от большинства млекопитающих кошки не воспринимают
+        сладкое, поскольку соответствующий ген у них повреждён, однако 
+        последние исследования опровергли эту информацию.</h1>
+        <div>Некоторые растения, например валериана или кошачья мята,
+        выделяют вещества, которые обычно оказывают на кошек 
+        (особенно на котов) воздействие, близкое к наркотическому. 
+        Впрочем, не все кошки реагируют 
+        на их запах, и не на всех кошек они оказывают одинаковое 
+        воздействие. У некоторых кошек валериана может вызвать отравление.</div>
+        <img src="''' + url_for('static', filename='cat.jpg') + '''">
     </body>
 </html>
 '''
