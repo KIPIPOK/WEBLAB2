6e77453679776c83d792a7be74af8e4c803ef3ac
София Лабаскина <sofialabaskina@gmail.com>
2023-10-26 20:34:13 +0700

    Форма

--- lab3.py
+++ lab3.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 lab3 = Blueprint('lab3', __name__)
 
-@lab3.route('/lab3/')
+@lab3.route('/lab3/form1')
 def lab():
-    return render_template('lab3.html')
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('form1.html', user=user, age=age, sex=sex)
