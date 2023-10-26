11719ecf5665e34de0809cc2b4b65f610de9695f
София Лабаскина <sofialabaskina@gmail.com>
2023-10-26 21:22:46 +0700

    Валидация

--- lab3.py
+++ lab3.py
@@ -3,7 +3,14 @@ lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/form1')
 def lab():
+    errors = {}
     user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+
     age = request.args.get('age')
+    if age == '':
+        errors['age'] = 'Заполните поле!'
+    
     sex = request.args.get('sex')
-    return render_template('form1.html', user=user, age=age, sex=sex)
+    return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
