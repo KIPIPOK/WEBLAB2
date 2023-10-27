267dc2e01a8452da24022a0023646623d2348d01
София Лабаскина <sofialabaskina@gmail.com>
2023-10-27 17:58:40 +0700

    Куки-цвет текста и самостоятельное задание

--- lab4.py
+++ lab4.py
@@ -86,3 +86,20 @@ def process_order():
         message = f"Заказ успешно сформирован. Вы заказали зерно: {grain_type}. Вес: {weight} т. Сумма к оплате: {total_price} руб."
 
     return f"<h3>{message}</h3><br><a href='/lab4/zerno'>Вернуться к заказу</a>"
+
+
+@lab4.route('/lab4/cookies', methods = ['GET', 'POST'])
+def cookies():
+    if request.method == 'GET':
+        return render_template('cookies.html' )
+    
+    color = request.form.get('color')
+    bgcolor = request.form.get('bgcolor')
+    fontsize = request.form.get('fontsize')
+    headers = {
+        'Set-Cookies': 'color=' + color + '; path=/',
+        'Set-Cookies': 'bgcolor=' + bgcolor + '; path=/',
+        'Set-Cookies': 'fontsize=' + fontsize + '; path=/',
+        'Location': '/lab4/cookies'
+    }
+    return '', 303, headers
